372e7fcb0275fed0ede299d0906114a016e23174
danho <danho011185@gmail.com>
2023-04-20 19:11:42 +0900

    add arrange.sh

---
+++ arrange.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd files
+mv a* A* ../a
+mv b* B* ../b
+mv c* C* ../c
+mv d* D* ../d
+mv e* E* ../e
+mv f* F* ../f
+mv g* G* ../g
+mv h* H* ../h
+mv i* I* ../i
+mv j* J* ../j
+mv k* K* ../k
+mv l* L* ../l
+mv m* M* ../m
+mv n* N* ../n
+mv o* O* ../o
+mv p* P* ../p
+mv q* Q* ../q
+mv r* R* ../r
+mv s* S* ../s
+mv t* T* ../t
+mv u* U* ../u
+mv v* V* ../v
+mv w* W* ../w
+mv x* X* ../x
+mv y* Y* ../y
+mv z* Z* ../z
